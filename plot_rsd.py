0b93d3a49d9b6ffa6887893c617b4c2f71b9e9a4
JackyYuenDacai <562422313@qq.com>
2021-01-01 21:13:05 +0800

    Update plot_rsd.py

--- plot_rsd.py
+++ plot_rsd.py
@@ -3,7 +3,9 @@ import matplotlib
 import matplotlib.pyplot as plt
 from scipy.ndimage.filters import gaussian_filter1d
 from scipy.signal import savgol_filter
-MAX_ENTRIES = 20000
+from dateutil.relativedelta import relativedelta
+
+MAX_ENTRIES = 30000
 SIGMA = 56
 WINDOW_SIZE = 7
 IF_SMOOTH = True
@@ -30,7 +32,7 @@ query = ("SELECT time_index, avg_amp, max_amp,min_amp FROM NAS order by time_ind
 cursor.execute(query)
 
 for (time_index, avg_amp, max_amp,min_amp) in cursor:
-	ti.append(time_index)
+	ti.append(time_index + relativedelta(hours=8))
 	avga.append(avg_amp)
 	maxa.append(max_amp)
 	mina.append(min_amp)
@@ -41,7 +43,7 @@ plt.plot_date(dates, smooth_data(maxa),linestyle='solid',marker='None')
 plt.plot_date(dates, smooth_data(mina),linestyle='solid',marker='None')
 plt.legend(('Average', 'Max', 'Min'),loc='upper right')
 plt.title('RSD record last '+str(MAX_ENTRIES)+' entries.')
-plt.xlabel("Time index (UTC)")
+plt.xlabel("Time index (HKT)")
 plt.show()
 cursor.close()
 cnx.close()
